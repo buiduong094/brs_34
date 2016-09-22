b2a8a6de75189fa896916d277fec50089d9f4d9f
buiduong094 <bui.van.duong@framgia.com>
2016-09-22 14:34:43 +0700

    Signup-signin-and-logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
     :trackable, :validatable
+  mount_uploader :avatar, AvatarUploader
   has_many :marks, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -13,4 +14,6 @@ class User < ActiveRecord::Base
     foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :name, presence: true
 end
