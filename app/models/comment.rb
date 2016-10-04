3035445b548e3ca7652564f1661cb41b33700631
buiduong094 <bui.van.duong@framgia.com>
2016-10-04 09:08:37 +0700

    user-manage-comment-on-review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
+  validates :content, presence: true, length: {maximum: 200}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,5 @@ Rails.application.routes.draw do
 
   resources :books
   resources :reviews
+  resources :comments
 end
